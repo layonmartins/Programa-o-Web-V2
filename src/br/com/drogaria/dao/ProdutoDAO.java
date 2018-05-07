806d68b64572615c171b655b7852e40330eb7e21
Layon Martins <layonmartins03@gmail.com>
2018-05-07 09:33:04 -0300

    aula 83 - Editando um Produto

--- src/br/com/drogaria/dao/ProdutoDAO.java
+++ src/br/com/drogaria/dao/ProdutoDAO.java
@@ -79,4 +79,22 @@ public class ProdutoDAO {
 			sessao.close();
 		}
 	}
+	
+	public void editar(Produto produto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			sessao.update(produto);
+			transacao.commit();
+		} catch (RuntimeException ex) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+			throw ex;
+		} finally {
+			sessao.close();
+		}
+	}
 }
--- src/br/com/drogaria/test/produtoDAOTest.java
+++ src/br/com/drogaria/test/produtoDAOTest.java
@@ -3,8 +3,6 @@ package br.com.drogaria.test;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -17,41 +15,59 @@ public class produtoDAOTest {
 
 	@Test
 	@Ignore
-	public void salvar(){
+	public void salvar() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscarPorCodigo(6L);
-		
+
 		Produto produto = new Produto();
 		produto.setDescricao("Shampo");
 		produto.setPreco(new BigDecimal(23.55D));
 		produto.setQuantidade(30);
 		produto.setFabricante(fabricante);
-		
+
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		produtoDAO.salvar(produto);
-		
+
 	}
-	
+
 	@Test
 	@Ignore
-	public void buscarPorCodigo(){
+	public void buscarPorCodigo() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		Produto produto = produtoDAO.buscarPorCodigo(3L);
 		System.out.println(produto);
 	}
-	
+
 	@Test
 	@Ignore
-	public void listar(){
+	public void listar() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		List<Produto> produtos = produtoDAO.listar();
 		System.out.println(produtos);
 	}
-	
+
 	@Test
-	public void excluir(){
+	@Ignore
+	public void excluir() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		Produto produto = produtoDAO.buscarPorCodigo(3L);
 		produtoDAO.excluir(produto);
 	}
+
+	@Test
+	@Ignore
+	public void editar() {
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		Produto produto = produtoDAO.buscarPorCodigo(2L);
+		produto.setDescricao("Mascara hidratenta");
+		produto.setPreco(new BigDecimal(6.37D));
+		produto.setQuantidade(10);
+		
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscarPorCodigo(8L);
+		produto.setFabricante(fabricante);
+		
+		produtoDAO.editar(produto);
+	}
+
 }
