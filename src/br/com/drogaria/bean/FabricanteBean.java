82aeae336d4b7e573adf8c892d522389a9671b55
Layon Martins <layonmartins03@gmail.com>
2018-05-12 16:29:32 -0300

    aula 94 - Inclusão do Fabricante

--- src/br/com/drogaria/bean/FabricanteBean.java
+++ src/br/com/drogaria/bean/FabricanteBean.java
@@ -3,13 +3,39 @@ package br.com.drogaria.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
 public class FabricanteBean {
 	
+	private Fabricante fabricanteCadastro;
+	
+	public Fabricante getFabricanteCadastro() {
+		if(fabricanteCadastro == null) {
+			fabricanteCadastro = new Fabricante();
+		}
+		return fabricanteCadastro;
+	}
+	public void setFabricanteCadastro(Fabricante fabricanteCadastro) {
+		this.fabricanteCadastro = fabricanteCadastro;
+	}
+
+	public void novo() {
+		fabricanteCadastro = new Fabricante();
+	}
+	
 	public void salvar() {
-		FacesUtil.addMsgInfo("Fabricante salvo com sucesso");
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.salvar(fabricanteCadastro);
+			fabricanteCadastro = new Fabricante();
+			FacesUtil.addMsgInfo("Fabricante salvo com sucesso");
+		} catch (Exception e) {
+			FacesUtil.addMsgErro("Erro ao tentar incluir um Fabricante: " + e.getMessage());
+		}
+		
 	}
 }
